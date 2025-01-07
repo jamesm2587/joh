4ed50514cd80b9b325890e57da064f891773fd9f
jamesm2587 <jamesmir123456@gmail.com>
2025-01-07 14:28:59 -0800

    Update streamlit_caption_generator_corrected.py

--- streamlit_caption_generator_corrected.py
+++ streamlit_caption_generator_corrected.py
@@ -21,7 +21,7 @@ st.markdown(
         transition: transform 0.2s ease;
     }
     .stButton>button:hover {
-        transform: scale(1.05);
+        transform: scale(5.05);
     }
     .stTextArea textarea {
         background: rgba(255, 255, 200, 0.8);
